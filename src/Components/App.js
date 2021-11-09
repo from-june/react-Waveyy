ca3f41a4da2c44fad5024271cde18a65b1c09bf2
Juun Pak <june.mintlatte@gmail.com>
2021-11-09 15:04:40 +0900

    📝 Create fundamental root

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Router from 'Components/Router';
+import GlobalStyles from 'Components/GlobalStyles';
+
+const App = () => (
+  <>
+    <Router />
+    <GlobalStyles />
+  </>
+);
+
+export default App;
